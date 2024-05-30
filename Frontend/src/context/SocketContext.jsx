441f483c64dc64be94044dcbe6ff622a9513f762
Sai Kumar Majji <saikumar170325@gmail.com>
2024-05-30 20:13:33 +0530

    socket url updated

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4000", {
+      const socket = io("https://mern-chat-app-ffxq.onrender.com", {
         query: {
           userId: authUser._id,
         },
