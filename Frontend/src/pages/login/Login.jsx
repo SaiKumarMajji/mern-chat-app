be148c73005473ea0ae4928671c421de684d3152
SaiKumarMajji <105416584+SaiKumarMajji@users.noreply.github.com>
2024-05-31 07:33:46 +0530

    Update Login.jsx

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           Login
-          <span className="text-blue-500"> Let's Talk</span>
+          <span className="text-blue-500"> ChatHub</span>
         </h1>
 
         <form onSubmit={handleSubmit}>
